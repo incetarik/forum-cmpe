0e6e16d9200edf8af9d0233d0ab08c0c46740ade
incetarik <incetarik@hotmail.com>
2019-04-08 12:36:33 +0300

    ~ `users.php` -> Used `db_helper.php`

--- db/users.php
+++ db/users.php
@@ -1,9 +1,9 @@
 <?php
 
-function get_all_users_raw($fields = '*') {
-    $db = get_db();
-    $result = $db->query("SELECT $fields FROM users;");
+require_once __DIR__ . '/db_helper.php';
 
+function get_all_users_raw($fields = '*') {
+    $result = safe_query("SELECT $fields FROM users;", true);
     return $result;
 }
 
@@ -16,9 +16,7 @@ function iterate_users($fields = '*') {
 }
 
 function get_by_id_raw($id, $fields = '*') {
-    $db = get_db();
-    $result = $db->query("SELECT $fields FROM users WHERE id = $id;");
-
+    $result = safe_query("SELECT $fields FROM users WHERE id = ?;", [$id], true);
     return $result;
 }
 
@@ -29,14 +27,16 @@ function get_by_id($id, $fields = '*') {
 
 function get_by_ids_raw($ids, $fields = '*') {
     $db = get_db();
-    if (is_array($ids)) {
-        $ids = implode(',', $ids);
+
+    if (is_string($ids)) {
+        $ids = explode(',', $ids);
     }
-    else if (!is_string($ids)) {
+    else if (!is_array($ids)) {
         throw new UnexpectedValueException('$ids should be string or array');
     }
 
-    $result = $db->query("SELECT $fields FROM users WHERE id IN $ids;");
+    $parameters = implode(',', array_fill(0, count($ids), '?'));
+    $result = safe_query("SELECT $fields FROM users WHERE id IN ($parameters);", $parameters, true);
 
     return $result;
 }
@@ -50,9 +50,7 @@ function iterate_users_by_id($ids, $fields = '*') {
 }
 
 function get_user_by_username_raw($username, $fields = '*') {
-    $db = get_db();
-    $result = $db->query("SELECT $fields FROM users WHERE username = '$username';");
-
+    $result = safe_query("SELECT $fields FROM users WHERE username = ?;", $username, true);
     return $result;
 }
 
@@ -62,17 +60,13 @@ function get_user_by_username($username, $fields = '*') {
 }
 
 function get_user_by_email($email, $fields = '*') {
-    $db = get_db();
-    $result = $db->query("SELECT $fields FROM users WHERE email_address = '$email';");
-
+    $result = safe_query("SELECT $fields FROM users WHERE email_address = ?;", $email, true);
     return $result;
 }
 
 function get_user_by_auth_raw($username, $password, $fields = '*') {
-    $db = get_db();
-    $results = $db->query("SELECT $fields FROM users WHERE username = '$username' AND password = SHA2('$password', 512);");
-
-    return $results;
+    $result = safe_query("SELECT $fields FROM users WHERE username = ? AND password = SHA2(?, 512);", [$username, $password], true);
+    return $result;
 }
 
 function get_user_by_auth($username, $password, $fields = '*') {
@@ -81,25 +75,22 @@ function get_user_by_auth($username, $password, $fields = '*') {
 }
 
 function is_username_available($username) {
-    $db = get_db();
-    $results = $db->query("SELECT username FROM users WHERE username = '$username' LIMIT 1;");
-    return !$results->num_rows;
+    $result = safe_query("SELECT username FROM users WHERE username = ? LIMIT 1;", $username, true);
+    return !$result->num_rows;
 }
 
 function is_email_available($email) {
-    $db = get_db();
-    $results = $db->query("SELECT email_address FROM users WHERE email_address = '$email';");
-    return !$results->num_rows;
+    $result = safe_query("SELECT email_address FROM users WHERE email_address = ?;", $email, true);
+    return !$result->num_rows;
 }
 
 function register_user_raw($username, $password, $mail, $name, $surname)
 {
-    $db = get_db();
-    $result = $db->query(<<<SQL
-    INSERT INTO users (username, password, email_address, name, surname) 
-    VALUES ('$username', SHA2('$password', 512), '$mail', '$name', '$surname');
+    $result = safe_query(<<<SQL
+    INSERT INTO users (username, password, email_address, name, surname)
+    VALUES (?, SHA2(?, 512), ?, ?, ?);
 SQL
-    );
+, [$username, $password, $mail, $name, $surname], true);
 
     return $result;
 }
