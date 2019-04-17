5374f57a0dd3808232b8c8685ab9811e6080adf5
incetarik <incetarik@hotmail.com>
2019-04-17 11:32:59 +0300

    ~ `profile` page, example usage of user session data

--- profile.php
+++ profile.php
@@ -1,5 +1,10 @@
-<?php include_once __DIR__ . '/layout/_layout_top.php'; ?>
+<?php include_once __DIR__ . '/layout/_layout_top.php';
 
+redirectToLoginIfNotLoggedIn();
+?>
 
+<div class="profile-page">
+    <h2 class="username"><?= $user['name'] ?> <?= $user['surname'] ?> <?= $user['email_address'] ?></h2>
+</div>
 
 <?php include_once __DIR__ . '/layout/_layout_bottom.php'; ?>
