6a21863d78d7751677bf1f692d22565ced94cdcf
incetarik <incetarik@hotmail.com>
2019-05-12 02:19:48 +0300

    ~ `style.scss` ~ `content` now showing the comments and user can add comments

--- assets/ts/entry.ts
+++ assets/ts/entry.ts
@@ -1,5 +1,14 @@
 namespace Entry {
   const awaitingLikeEntries = []
+  let replyButton: HTMLAnchorElement
+  let titleInput: HTMLInputElement
+  let contentInput: HTMLInputElement
+
+  Global.extendOnLoad(() => {
+    replyButton = document.getElementById('reply') as HTMLAnchorElement
+    titleInput = document.getElementById('title') as HTMLInputElement
+    contentInput = document.getElementById('content') as HTMLInputElement
+  })
 
   export function toggleLike(entryId: number, callback?: (state: 'like' | 'dislike') => void) {
     if (~awaitingLikeEntries.indexOf(entryId)) return
@@ -19,5 +28,52 @@ namespace Entry {
         button.innerText = result.data[0] == 'd' ? 'Like' : 'Dislike'
       }
     }))
+  }
+
+  export function addComment(title: string, content: string, entryId: number, userId: number, userFullName: string, jobTitle: string) {
+    Global.post('/api/entries.php?f=add_comment', {
+      entry: entryId,
+      title,
+      content
+    }, (result) => {
+      if (result && result.data > 0) {
+        const article = createArticleElement(title, content, entryId, userId, userFullName, jobTitle)
+        replyButton.parentElement.parentElement.before(article)
+        if (titleInput) titleInput.value = ''
+        if (contentInput) contentInput.value = ''
+      }
+    })
+  }
+
+  function createArticleElement(title: string, content: string, entryId: number, userId: number, userFullName: string, jobTitle: string) {
+    const article = document.createElement('article')
+    article.className = 'article'
+    const personDiv = document.createElement('div')
+    personDiv.className = 'person'
+    const personImg = document.createElement('img')
+    personImg.src = '/assets/img/avatar/' + userId + '.jpg'
+    personImg.alt = userFullName
+    const h3 = document.createElement('h3')
+    h3.innerText = userFullName
+    let p = document.createElement('p')
+    p.innerText = jobTitle
+
+    personDiv.append(personImg)
+    personDiv.append(h3)
+    personDiv.append(p)
+
+    const textDiv = document.createElement('div')
+    if (title) {
+      const text = document.createElement('h2')
+      text.innerText = title
+      textDiv.append(text)
+    }
+
+    p = document.createElement('p')
+    p.innerText = content
+    textDiv.append(p)
+    article.append(personDiv)
+    article.append(textDiv)
+    return article
   }
 }
--- content.php
+++ content.php
@@ -5,68 +5,113 @@ $entry = null;
 $categories = null;
 $tags = null;
 $userLiked = false;
+$comments = [];
 
 if (isset($_GET['eid'])) {
     $entry_id = $_GET['eid'];
 }
 
 if ($entry_id) {
-  $entry = get_entry_by_id(intval($entry_id));
-  $categories = $entry['categories'];
-  $categories = explode(';', $categories);
-  $tags = $entry['tags'];
-  $tags = explode(';', $tags);
-  $userLiked = get_has_user_liked_entry($user['id'], $entry['id']);
+    $entry = get_entry_by_id(intval($entry_id));
+    $categories = $entry['categories'];
+    $categories = explode(';', $categories);
+    $tags = $entry['tags'];
+    $tags = explode(';', $tags);
+    $userLiked = get_has_user_liked_entry($user['id'], $entry['id']);
+    $comments = get_comments_of_entry($entry['id']);
 }
 
 ?>
 
 <div class="main">
   <div class="container">
-    <?php if ($entry): ?>
-    <section class="content">
-      <!-- TODO: LIKE COUNT -->
-      <?php if ($categories and sizeof($categories)): ?>
-      <div class="head">
-        <h1><?= $categories[0] ?></h1>
-      </div>
-      <?php endif; ?>
-      <article class="article">
-        <div class="person">
-          <img src="assets/img/avatar/<?= $entry['created_by'] ?>.jpg" alt=""/>
-          <h3><?= $entry['name'] ?> <?= $entry['surname'] ?></h3>
-          <p><?= $entry['job_title'] ?></p>
-        </div>
-        <div class="text">
-          <h2><?= $entry['title'] ?></h2>
-          <p><?= $entry['content'] ?></p>
-          <?php if ($tags and sizeof($tags)): ?>
-          <p>
-            <?php foreach ($tags as $tag): ?>
-            <span>
+      <?php if ($entry): ?>
+        <section class="content">
+          <!-- TODO: LIKE COUNT -->
+            <?php if ($categories and sizeof($categories)): ?>
+              <div class="head">
+                <h1><?= $categories[0] ?></h1>
+              </div>
+            <?php endif; ?>
+          <article class="article">
+            <div class="person">
+              <img src="assets/img/avatar/<?= $entry['created_by'] ?>.jpg" alt=""/>
+              <h3><?= $entry['name'] ?> <?= $entry['surname'] ?></h3>
+              <p><?= $entry['job_title'] ?></p>
+            </div>
+            <div class="text">
+              <h2><?= $entry['title'] ?></h2>
+              <p><?= $entry['content'] ?></p>
+                <?php if ($tags and sizeof($tags)): ?>
+                  <p>
+                      <?php foreach ($tags as $tag): ?>
+                        <span>
               <!-- TODO: Styling -->
               <a href="/search-result.php?tag=<?= urlencode($tag) ?>"><?= $tag ?></a>
             </span>
-            <?php endforeach; ?>
-          </p>
-          <?php endif; ?>
+                      <?php endforeach; ?>
+                  </p>
+                <?php endif; ?>
 
-          <?php if ($user and ($user['id'] != $entry['created_by'])): ?>
-          <div class="like">
-            <button id="like_button_<?= $entry['id'] ?>" class="active" onclick="Entry.toggleLike(<?= $entry['id'] ?>)">
-              <?= $userLiked ? 'Dislike' : 'Like' ?>
-            </button>
-          </div>
-          <?php endif; ?>
-        </div>
-      </article>
-    </section>
+                <?php if ($user and ($user['id'] != $entry['created_by'])): ?>
+                  <div class="like">
+                    <button id="like_button_<?= $entry['id'] ?>" class="active"
+                            onclick="Entry.toggleLike(<?= $entry['id'] ?>)">
+                        <?= $userLiked ? 'Dislike' : 'Like' ?>
+                    </button>
+                  </div>
+                <?php endif; ?>
+            </div>
+              <?php foreach ($comments as $comment): ?>
+                <article class="article">
+                  <div class="person">
+                    <img src="/assets/img/avatar/<?= $comment['userid'] ?>.jpg"
+                         alt="<?= $comment['name'] ?> <?= $comment['surname'] ?>"/>
+                    <h3><?= $comment['name'] ?> <?= $comment['surname'] ?></h3>
+                    <p><?= $comment['job_title'] ?></p>
+                  </div>
+                  <div class="text">
+                      <?php if (isset($comment['title']) && $comment['title']): ?>
+                        <h2><?= $comment['title'] ?></h2>
+                      <?php endif; ?>
+                    <p><?= $comment['content'] ?></p>
+                  </div>
+                </article>
+              <?php endforeach; ?>
+              <?php if (isset($user) and $user): ?>
+                <article class="article reply">
+                  <form action="#">
+                    <h1>Enter message title and the content</h1>
+                    <div class="form-group">
+                      <input type="text" placeholder="Message Title" name="title" id="title">
+                    </div>
+                    <div class="form-group">
+                      <textarea placeholder="Message Content" name="content" id="content"></textarea>
+                    </div>
+                    <p>Defamation, racism, religion, language, the separation of the region, including politics,
+                      manipulation, humiliation messages will not be allowed, and those who make such shares will be
+                      followed by our system and their membership will be canceled.</p>
+                    <a class="send" id="reply" href="javascript:Entry.addComment(title.value, content.value, <?=
+                    $entry['id']
+                    ?>, <?=
+                    $user['id']
+                    ?>, '<?=
+                    $user['name'] . ' ' . $user['surname']
+                    ?>', '<?=
+                    $user['job_title']
+                    ?>')"
+                       type="button">Send</a>
+                  </form>
+                </article>
+              <?php endif; ?>
+          </article>
+        </section>
 
-    <?php else: ?>
-      <!-- TODO: NOT FOUND -->
-    <?php endif; ?>
+      <?php else: ?>
+        <!-- TODO: NOT FOUND -->
+      <?php endif; ?>
 
-    <?php include_once __DIR__ . '/layout/_sidebar.php'; ?>
+      <?php include_once __DIR__ . '/layout/_sidebar.php'; ?>
   </div>
 </div>
 
