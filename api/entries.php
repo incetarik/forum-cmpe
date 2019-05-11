fadc30532374281b86ef6509188e64ec8074d454
incetarik <incetarik@hotmail.com>
2019-05-12 02:19:04 +0300

    + `api/entries/get_entry_comments` + `api/entries/add_comment`

--- api/entries.php
+++ api/entries.php
@@ -118,13 +118,52 @@ function _entry_did_user_like($params) {
     return get_has_user_liked_entry($user['id'], intval($params['entry']));
 }
 
+function _entry_get_comments($params) {
+    if (!array_key_exists('entry', $params)) {
+        var_dump($params);
+        throw new Error('Invalid parameters, "entry" expected');
+    }
 
+    $entry_id = intval($params['entry']);
+    if (isset($params['user'])) {
+        $user_id = intval($params['user']);
+        return get_comments_of_entry_by_user($user_id, $entry_id);
+    }
+
+    return get_comments_of_entry($entry_id);
+}
+
+function _entry_add_comment($params) {
+    $user = get_user();
+
+    if (!$user) {
+        throw new Error('User not found');
+    }
+
+    if (!array_key_exists('entry', $params)) {
+        var_dump($params);
+        throw new Error('Invalid parameters, "entry" expected');
+    }
+
+    if (!array_key_exists('title', $params)) {
+        var_dump($params);
+        throw new Error('Invalid parameters, "title" expected');
+    }
+
+    if (!array_key_exists('content', $params)) {
+        var_dump($params);
+        throw new Error('Invalid parameters, "content" expected');
+    }
+
+    return add_comment_to_entry($user['id'], intval($params['entry']), $params['title'], $params['content']);
+}
 
 register_handler('like_entry', '_entry_like', 'p');
 register_handler('dislike_entry', '_entry_dislike', 'p');
 register_handler('create_entry', '_entry_create', 'p');
-register_handler('toggle_like_entry', '_entry_like_toggle');
-register_handler('user_did_like', '_entry_did_user_like');
-
+register_handler('toggle_like_entry', '_entry_like_toggle', 'p');
+register_handler('user_did_like', '_entry_did_user_like', 'p');
+register_handler('get_entry_comments', '_entry_get_comments', 'p');
+register_handler('add_comment', '_entry_add_comment');
 
 handle_request();
